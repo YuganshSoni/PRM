4dbdbca590ad8e96800ea34c0fb3dab113bbfa7a
yugansh <yugansh.soni@visionarytechservices.com>
2026-06-22 23:35:04 +0530

    document environment setup and end-to-end run instructions

--- client/config.py
+++ client/config.py
@@ -6,6 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class ClientSettings(BaseSettings):
     api_base_url: str = "http://localhost:8000"
     request_timeout_seconds: float = 30.0
+    ai_request_timeout_seconds: float = 90.0
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
